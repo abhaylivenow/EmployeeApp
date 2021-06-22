3a31b915378f13881d143d22714e7fb87150db3d
Harsh dangi <harshd941@gmail.com>
2021-06-22 17:38:47 +0530

    Firebase authentication added

---
+++ app/src/main/java/com/example/internprojectday1/ApplicationClass.java
@@ -0,0 +1,14 @@
+package com.example.internprojectday1;
+
+import android.app.Application;
+import android.content.Intent;
+
+public class ApplicationClass extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        startActivity(new Intent(this,OtpVerification.class));
+    }
+}
---
+++ app/src/main/java/com/example/internprojectday1/OtpVerification.java
@@ -0,0 +1,86 @@
+package com.example.internprojectday1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class OtpVerification extends AppCompatActivity {
+
+    Button getotpbutton;
+    EditText enternumber;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_verification);
+
+        enternumber=findViewById(R.id.input_mobile_number);
+        getotpbutton=findViewById(R.id.buttongetotp);
+
+
+        ProgressBar progressBar=findViewById(R.id.progressbarsendingotp);
+
+        getotpbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!enternumber.getText().toString().isEmpty()){
+                    if((enternumber.getText().toString().trim()).length()==10){
+
+                        progressBar.setVisibility(View.VISIBLE);
+                        getotpbutton.setVisibility(View.GONE);
+
+                        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                                "+91" + enternumber.getText().toString(), 30,
+                                TimeUnit.SECONDS,
+                                OtpVerification.this,
+                                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                                    @Override
+                                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                        progressBar.setVisibility(View.GONE);
+                                        getotpbutton.setVisibility(View.VISIBLE);
+                                    }
+
+                                    @Override
+                                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                                        progressBar.setVisibility(View.GONE);
+                                        getotpbutton.setVisibility(View.VISIBLE);
+                                        Toast.makeText(OtpVerification.this, "Error!"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    @Override
+                                    public void onCodeSent(@NonNull String backendotp, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                        progressBar.setVisibility(View.GONE);
+                                        getotpbutton.setVisibility(View.VISIBLE);
+                                        Intent intent=new Intent(getApplicationContext(),verifyenterotp.class);
+                                        intent.putExtra("mobile",enternumber.getText().toString());
+                                        intent.putExtra("backendotp",backendotp);
+                                        startActivity(intent);
+                                    }
+                                }
+                        );
+                        Intent intent=new Intent(getApplicationContext(),verifyenterotp.class);
+                        intent.putExtra("mobile",enternumber.getText().toString());
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(OtpVerification.this, "Please enter correct number", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(OtpVerification.this, "Enter mobile number", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
